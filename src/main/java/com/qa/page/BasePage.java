523f4e70bc77551959751bbc248930133265b0ba
kamalinijayaprakash <kamalini.jayaprakash@gmail.com>
2020-02-09 15:16:16 -0800

    Pagebranch (#6)
    * Add function
    
    * sub function
    
    * mul function

--- src/main/java/com/qa/page/BasePage.java
+++ src/main/java/com/qa/page/BasePage.java
@@ -7,4 +7,13 @@ public BasePage(){
     public BasePage(int a){
         System.out.println("I am in base page");
     }
+    public void add(){
+        System.out.println("I am add");
+    }
+    public void sub(){
+        System.out.println("I am sub");
+    }
+    public void mul(){
+        System.out.println("I am mul");
+    }
 }
